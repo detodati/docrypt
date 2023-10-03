0dc3e5de0c76073ab478f173223eea56fe76973a
Samuele de Tomasi <el3um4s@gmail.com>
2023-10-03 23:06:16 +0200

    Delete Components directory

--- Components/UI/Languages/Lang.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import lang from "./Lang";
-  import { languages } from "./Languages";
-
-  export let p: string;
-  export let w: string;
-</script>
-
-{@html languages[p][w][$lang]}
